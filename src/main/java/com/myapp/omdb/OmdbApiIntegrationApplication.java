30279155b70789a4af0e92aad97dcc7688fa441d
joshua <joshuasainz95@gmail.com>
2023-08-14 15:49:37 +0200

    Open Movie Database bug tracking API Integration Project started - Added dependencies (maven) : Spring Web, JPA, Thymeleaf, MySQL, DevTools and Lombok

---
+++ src/main/java/com/myapp/omdb/OmdbApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.omdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmdbApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/omdb/OmdbApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.omdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmdbApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
