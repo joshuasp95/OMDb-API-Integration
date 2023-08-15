26a951e3c4419ee9f883dda10a24101bdcbd8314
joshua <joshuasainz95@gmail.com>
2023-08-15 15:25:59 +0200

    Design and implementation of Movie model done, and some comments added to .properties file

--- src/main/java/com/myapp/omdb/persistence/entity/Movie.java
+++ src/main/java/com/myapp/omdb/persistence/entity/Movie.java
@@ -1,8 +1,33 @@
 package com.myapp.omdb.persistence.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "movie")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Movie {
+
+    @Id
+    @Column(name = "imdb_id", nullable = false)
+    private String imdbId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String year;
+
+    private String image; // Could be null for movies without an image
+
+    @Column(name = "personal_rating")
+    private Integer personalRating;
 
 }
